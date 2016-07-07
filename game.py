49e6b3561852f9a68dcf5c879f687b00bdfe47cc
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 18:01:21 -0700

    added try and except functions to handle cases where user guess is not a number.

--- game.py
+++ game.py
@@ -8,13 +8,22 @@ name = raw_input("Enter your name: ")
 print "Welcome %s, I'm thinking of a number 1 - 100, try to guess it!" % name
 #Randint is a function that pulls a random number between 2 bounds from random module
 answer = random.randint(0, 100)
+
+
+
 #User inputs answer, transform string into integer
-guess = int(raw_input("Guessed number: "))
-
-
+#guess = int(raw_input("Guessed number: "))
 
 #While loops sets a condidtion that won't stop until guess equals answer
 while True:
+    #Try and except functions handle cases where guesses are not numbers
+    try:
+        guess = int(raw_input("Guessed number: "))
+    except ValueError:
+        print "Please enter an integer..."
+        continue
+        #guess = int(raw_input("Guessed number: "))
+
     #Bounds the users' guess between 0 and 100
     if guess < 0 or guess > 100:
         print "Learn how to count! Enter a new number between 0 - 100"
